67fa844779d5c17ea9e50e171ca944f695002343
scarabush_scarabush <snusikfrescus@gmail.com>
2024-09-28 05:16:53 +0300

    adding some

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-use std::io;
-
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("msg");
-    let n: i32 = input.trim().parse().expect("msg");
+    let p0: f32 = 1.25;
+    let z: f32 = 1.21*10.0f32.powf(-4.0f32);
     
-    for i in 1..=n {
-        for j in 1..=i {
-            print!("*");
-        }
-        println!("")
+    println!("Высота (м)\tПлотность (кг/м^3)");
+    for h in (0..1000).step_by(100) {
+        let p = p0 * (-h as f32 * z).exp();
+        println!("{}\t\t{}", h, p)
     }
 }
 
