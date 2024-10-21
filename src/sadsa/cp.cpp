52fdea3d7f1902bf32ad24b18e89144bf63000a2
scarabush_scarabush <snusikfrescus@gmail.com>
2024-10-21 21:12:06 +0300

    123

--- src/sadsa/cp.cpp
+++ src/sadsa/cp.cpp
@@ -1,59 +1,60 @@
-#include <cstdlib>
 #include <iostream>
+#include <memory>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    
-    int row = 10;
-    int column = 10;
-    
-    vector<vector<int>> matrix(row, vector<int>(column));
-   
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-            matrix[i][j] = rand() % 10+1;
-        }
-    } 
-   
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-            cout<<matrix[i][j]<< " ";
-        }
-        cout<<"\n";
-    } 
-   
-    cout<<"\n";
-   
+    int row = 5;
+    int column = 5;
 
+    vector<vector<int>> matrix(row, vector<int>(column));
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+          matrix[i][j] = rand() % 10 + 1;
+        }
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+          cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+  
     int multiplication = 1;
+    
     for (int i = 0; i < row; i++) {
         multiplication *= matrix[i][i];
     }
-    cout<<multiplication<<endl;
-    
+    cout << multiplication << endl;
+
+    auto a = make_unique<int>(5);
+
+    multiplication = 1;
     for (int i = 0; i < row; i++) {
-        multiplication *= matrix[i][i - row - 1];
+        multiplication *= matrix[i][row - 1 - i];
     }
-    cout<<multiplication<<endl;
-   
-    
+    cout << multiplication << endl;
+
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < column; j++) {
+        if (i > j) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++) {
-           if (i > j) {
-               matrix[i][j] = 0;
-           }
-        }
+          cout << matrix[i][j] << " ";
     }
-    
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-           cout<<matrix[i][j]<< " ";
-        }
-        cout<<"\n";
-    } 
-    
+      cout << "\n";
+    }
 
     return 0;
-}
+  }
